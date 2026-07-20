27903ec48d93021f34c4ee2bdc0b4f1ea078327b
rajeev190862-crypto <rajeev190862@gmail.com>
2026-07-20 19:14:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,19 @@
-*{
-    margin:0;
-    padding:0;
-    box-sizing:border-box;
-}
+const music = document.getElementById("bgMusic");
+const musicBtn = document.getElementById("musicBtn");
 
-body{
-    background:#050816;
-    color:white;
-    font-family:Arial, sans-serif;
-    height:100vh;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    text-align:center;
-    overflow:hidden;
-}
+music.volume = 0.3;
 
-.welcome{
-    padding:20px;
-}
+musicBtn.addEventListener("click", function () {
 
-h1{
-    font-size:55px;
-    color:#ff4d88;
-    margin-bottom:20px;
-}
+    if (music.paused) {
+        music.play();
+        musicBtn.innerHTML = "⏸️";
+    } else {
+        music.pause();
+        musicBtn.innerHTML = "🎵";
+    }
 
-p{
-    font-size:22px;
-    color:#ddd;
-    line-height:1.7;
-    margin-bottom:40px;
-}
+});
 
-button{
-    background:#ff4d88;
-    color:white;
-    border:none;
-    padding:15px 35px;
-    font-size:20px;
-    border-radius:50px;
-    cursor:pointer;
-    transition:0.3s;
-}
 
-button:hover{
-    transform:scale(1.08);
-}
+ 
