e5212716d34fae01e83eefeacdc5ac64129ded73
rajeev190862-crypto <rajeev190862@gmail.com>
2026-07-20 19:05:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+}
 
+body{
+    background:#050816;
+    color:white;
+    font-family:Arial, sans-serif;
+    height:100vh;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    text-align:center;
+    overflow:hidden;
+}
+
+.welcome{
+    padding:20px;
+}
+
+h1{
+    font-size:55px;
+    color:#ff4d88;
+    margin-bottom:20px;
+}
+
+p{
+    font-size:22px;
+    color:#ddd;
+    line-height:1.7;
+    margin-bottom:40px;
+}
+
+button{
+    background:#ff4d88;
+    color:white;
+    border:none;
+    padding:15px 35px;
+    font-size:20px;
+    border-radius:50px;
+    cursor:pointer;
+    transition:0.3s;
+}
+
+button:hover{
+    transform:scale(1.08);
+}
